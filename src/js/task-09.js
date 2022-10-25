80679f2cd5e3aeeef8580d8ca0adb912056fa187
Oksana-Zakhariia <okcahka.l@gmail.com>
2022-10-25 19:29:58 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonEl = document.querySelector('.change-color')
+const spanEl = document.querySelector('.color')
+
+buttonEl.addEventListener('click', onButtonClick)
+function onButtonClick() {
+  const color = getRandomHexColor()
+  document.querySelector('body').style.backgroundColor = color;
+  spanEl.textContent = color
+}
